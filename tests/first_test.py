df75f4b625c80a6105ede3859a8ea9caa3d5d698
Дамир Сабиров <damir-sk>
2021-09-01 12:34:24 +0400

    к 19.2.3, к 19.7.2

---
+++ tests/first_test.py
@@ -0,0 +1,40 @@
+import pytest
+
+from app.Calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_calculation_failed(self):
+        assert self.calc.multiply(self, 2, 2) ==5
+
+#**************************
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 2, 2) == 1
+
+    def test_division_calculation_failed(self):
+        assert self.calc.division(self, 2, 0) == False
+
+
+#**************************
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 5, 6) == 11
+
+    def test_adding_calculation_failed(self):
+        assert self.calc.adding(self, 2, 0) == 5
+
+#**************************
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 5, 6) == -1
+
+    def test_subtraction_calculation_failed(self):
+        assert self.calc.subtraction(self, 5, 3) == 5
